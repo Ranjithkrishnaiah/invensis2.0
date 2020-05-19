2e7e521ef948c3ff269308d66d4e1eedd31f3d70
ranjithkrishnaiah <ranjithksow@gmail.com>
2020-05-19 19:40:46 +0530

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,43 @@
 // navbar fixed on top script
-!(function($) {
+! function(e) {
   "use strict";
-
-  // Toggle .navbar-shrink class to #header when page is scrolled
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 50) {
-      $('#mainNav').addClass('navbar-shrink');
-    } else {
-      $('#mainNav').removeClass('navbar-shrink');
-    }
+  e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+          var a = e(this.hash);
+          if ((a = a.length ? a : e("[name=" + this.hash.slice(1) + "]")).length) return e("html, body").animate({
+              scrollTop: a.offset().top - 54
+          }, 1e3, "easeInOutExpo"), !1
+      }
+  }), e(".js-scroll-trigger").click(function() {
+      e(".navbar-collapse").collapse("hide")
+  }), e("body").scrollspy({
+      target: "#mainNav",
+      offset: 56
   });
 
-  if ($(window).scrollTop() > 50) {
-    $('#header').addClass('navbar-shrink');
+  function a() {
+      10 < e("#mainNav").offset().top ? e("#mainNav").addClass("navbar-shrink") : e("#mainNav").removeClass("navbar-shrink")
   }
+  a(), e(window).scroll(a)
+}(jQuery);
 
-})(jQuery);
+// !(function($) {
+//   "use strict";
+
+//   // Toggle .navbar-shrink class to #header when page is scrolled
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() > 50) {
+//       $('#mainNav').addClass('navbar-shrink');
+//     } else {
+//       $('#mainNav').removeClass('navbar-shrink');
+//     }
+//   });
+
+//   if ($(window).scrollTop() > 50) {
+//     $('#header').addClass('navbar-shrink');
+//   }
+
+// })(jQuery);
 
 
 // search full screen
